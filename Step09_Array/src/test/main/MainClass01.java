5c7780c02ef988fac21dd9b82304c60d9e543397
Heojoeun <joeun424@gmail.com>
2017-11-20 17:38:03 +0900

    Array 완료!

---
+++ Step09_Array/src/test/main/MainClass01.java
@@ -0,0 +1,22 @@
+package test.main;
+
+public class MainClass01 {
+	public static void main(String[] args) {
+		/*
+		 * 	java 에서 Array 은 한 type 만 집어넣을 수 있다.
+		 * 	처음 만든 방의 갯수를 변경할 수 없다.
+		 * 	처음 배열을 만든 시점에 방에는 기본데이터 타입을 갖는 배열에는 0이 들어 있다.
+		 * 	처음 배열을 만든 시점에 방에는 참조데이터 타입을 갖는 배열에는 nuul 이 들어 있다.
+		 */
+		
+		// int type 을 저장할 수 있는 방 3개 짜리 배열객체 생성
+		int[] nums = new int[3];
+		
+		// double type 을 저장할 수 있는 방 5개 짜리 배열객체 생성
+		double[] nums2 = new double[5];
+		
+		// String type 을 저장할 수 있는 방3개 짜리 배열객체를
+		// 생성하고 각각의 방에 뭐가 들어 있는지 확인해 보세요.
+		String[] msgs = new String[3];
+	}
+}
